937761fb109914a6d09bd900af63ad47f22f2062
UwUssimo Robinson <uwussimo@icloud.com>
2022-06-29 15:01:18 +0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,15 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 function index (_req: IncomingMessage, res: ServerResponse) {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Uzinfocom open graph image generator<h1/>')
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        paths: [
+            {
+                path: "/honor",
+                params: ["full_name"]
+            }
+        ]
+    }))
 }
 
 export default index;
