8f3152ab3b24a0f1bc72ce89f8e5219c4a630179
UwUssimo Robinson <uwussimo@icloud.com>
2022-08-12 18:02:18 +0500

    adding warrior

---
+++ api/warrior/_lib/parser.ts
@@ -0,0 +1,20 @@
+import { IncomingMessage } from "http";
+import { parse } from "url";
+import { ParsedRequest } from "./types";
+
+export function parseRequest(req: IncomingMessage) {
+  const { query } = parse(req.url || "/", true);
+  const { full_name } = query;
+
+  if (Array.isArray(full_name)) {
+    throw new Error("Expected a single full name");
+  }
+
+  const parsedRequest: ParsedRequest = {
+    full_name: full_name,
+  };
+
+  return parsedRequest;
+}
+
+export default parseRequest;
